483ec39107a6d33e64257c6cd63bc09fad96bb0a
Leandro Paiva <leandroapaiva@yahoo.com>
2022-07-27 21:07:04 -0300

    ✔️ feat: Req02 - Cria uma QUERY que exibe três colunas: canções, artistas e albuns

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,11 @@
+SELECT 
+COUNT(DISTINCT cancoess.cancoes) AS cancoes, 
+COUNT(DISTINCT artistass.artista_id) AS artistas, 
+COUNT(DISTINCT albunss.album_id) AS albuns
+	FROM 
+		SpotifyClone.cancoes_table AS cancoess
+	INNER JOIN 
+        SpotifyClone.album_table AS albunss ON albunss.album_id = cancoess.album_id
+	INNER JOIN 
+        SpotifyClone.artistas_table AS artistass ON artistass.artista_id = albunss.artista_id;
 
