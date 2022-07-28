8219c99e4362031aa1566445df3c8dae975c211b
Leandro Paiva <leandroapaiva@yahoo.com>
2022-07-28 01:23:25 -0300

    ✔️ feat: Req 4 - Exibe a condicao do usuario se esta ativo ou inativo

--- desafio3.sql
+++ desafio3.sql
@@ -5,9 +5,11 @@ ROUND(SUM(cancoess.duracao_segundos /60), 2) AS total_minutos
 	FROM 
 		SpotifyClone.usuario_table AS usuarioss
 	INNER JOIN 
-        SpotifyClone.reproducoes_table AS reproducoess ON usuarioss.usuario_id = reproducoess.usuario_id
+        SpotifyClone.reproducoes_table AS reproducoess 
+      ON usuarioss.usuario_id = reproducoess.usuario_id
 	INNER JOIN     
-        SpotifyClone.cancoes_table AS cancoess ON cancoess.cancao_id = reproducoess.historico_de_reproducoes
+        SpotifyClone.cancoes_table AS cancoess 
+      ON cancoess.cancao_id = reproducoess.historico_de_reproducoes
 	GROUP BY usuario;
 
 -- fonte: https://www.w3schools.com/sql/func_mysql_round.asp
---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT usuarioss.usuario AS usuario,
+IF (YEAR(MAX(reproducoess.data_reproducao)) = 2021,
+  "Usuário ativo", "Usuário inativo") AS condicao_usuario
+FROM
+  SpotifyClone.usuario_table AS usuarioss
+INNER JOIN 
+  SpotifyClone.reproducoes_table AS reproducoess ON usuarioss.usuario_id = reproducoess.usuario_id
+GROUP BY usuario ASC;
+
